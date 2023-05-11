e2b593178aa213c072f8a1086552783a655e6277
Kamil <mentos.96@o2.pl>
2023-05-11 21:48:28 +0200

    Write main funcjonality prgram

--- Program.cs
+++ Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wcisnij 1 (Lucek) Wcisnij 2 (Laura) Wcisnij 3 prezstawienie");
+            Elephant Lucek = new Elephant( "Lucek",100);
+            Elephant Laura = new Elephant("Laura",80);
+            while (true)
+            {
+                Console.WriteLine("Wcisnij 1 (Lucek) Wcisnij 2 (Laura) Wcisnij 3 (prezstawienie)");
+                char presedKey = Console.ReadKey(true).KeyChar;
+                Console.WriteLine("Wcisnoles przycisk: " +  presedKey);
+                if (presedKey == '1')
+                {
+                    Console.WriteLine("Wywolanie Lucek.WhoAmI");
+                    Lucek.WhoAmI();
+                }
+                else if(presedKey == '2')
+                {
+                    Console.WriteLine("Wywolanie Laura.WhoAmI");
+                    Laura.WhoAmI();
+                }
+                else if(presedKey == '3')
+                {
+                    Elephant teporary = Lucek;
+                    Laura = Lucek;
+                    Lucek = teporary;
+                    Console.WriteLine("Referencje zostaly przestawione");
+                }
+
+                Console.WriteLine("Wcisnij K aby kontynulowac lub inny klawisz aby zakonczyć");
+                presedKey = Console.ReadKey(true).KeyChar;
+                if (presedKey == 'K' || presedKey == 'k') { }
+                else return;
+
+
+            }
         }
     }
 }
