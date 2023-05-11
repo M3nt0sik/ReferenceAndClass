816329a6e3a962d01c8ae4bd481acff264be3435
Kamil <mentos.96@o2.pl>
2023-05-11 21:27:00 +0200

    Dodaj pliki projektów.

---
+++ Elephant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceAndClass
+{
+    internal class Elephant
+    {
+        public string Name = "";
+        public int EarSize;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine("Na imie mam: " + Name);
+            Console.WriteLine("Dulgosc moich uszu to: " + EarSize + " cm");
+        }
+    }
+}
